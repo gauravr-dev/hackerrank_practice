877be23d5fa7ecc0a011ae6e1be695fe7a613877
Gaurav Rajput <gaurav.rajput@ebizontek.com>
2017-01-02 14:52:40 +0530

    Binary number problem solved.

--- binary.swift
+++ binary.swift
@@ -1,14 +1,21 @@
 
-print("Enter a number")
+// print("Enter a number")
 
 var number = Int(readLine(stripNewline : true)!)!
 var count = 0
+var maxCount = 0
+var rem = 0
 while(number > 0){
-    if number % 2 == 0{
-        continue
-    } 
-    count += 1
+	rem = number % 2
+    if rem > 0 {
+    	count += 1	
+    	if count > maxCount {
+   			maxCount = count	
+    	}
+    }else {    	
+    	count = 0
+    }
     number /= 2
 }
 
-print(count)
+print(maxCount)
